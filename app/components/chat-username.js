ee0f1d0b3d67a6ce90b98b9a46e4c3df22004f56
emeryparks <emeryparks09@hotmail.com>
2018-12-13 22:19:35 -0800

    chat app functioning properly

---
+++ app/components/chat-username.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+export default Component.extend({
+  activeUserService: service('active-user'),
+  actions: {
+    userSubmittedName() {
+      const user = this.get('userName');
+      this.get('activeUserService').setUser(user);
+    }
+  }
+});
